0c75ee19f55b8dbd3688c70611cabb1f98b461a5
insideplant <kami.10pearl@gmail.com>
2021-09-23 15:44:34 +0000

    start add profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'users/show'
+    get 'users/index'
+    get 'users/edit'
+    get 'users/update'
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   scope module: :public do
     root to: 'homes#top'
+    resources :users
   end
 end
