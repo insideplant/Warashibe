9b21f3ef5fb8787a0049c0d481be87f9d736e2e1
insideplant <kami.10pearl@gmail.com>
2021-09-24 15:42:06 +0000

    make album

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-
-  namespace :public do
-    get 'users/show'
-    get 'users/index'
-    get 'users/edit'
-    get 'users/update'
-  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
