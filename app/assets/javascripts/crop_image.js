40f7c0cd619d0e24c734af10fe07b001fdd3b7ee
insideplant <kami.10pearl@gmail.com>
2021-09-28 15:29:08 +0000

    add ajax

--- app/assets/javascripts/crop_image.js
+++ app/assets/javascripts/crop_image.js
@@ -2,140 +2,113 @@
 /*global Cropper*/
 
 $(function () {
-  // let image = document.getElementById('imageModal');
-  // let img_field = document.getElementById('item_image');
-  // let cropModal = document.getElementById('modal3');
-  // let beforeUpload = document.getElementById('beforeUpload');
-  // let button = document.getElementById('btn-save');
-
   let cropperImg = document.getElementById('cropper-img');
   const btn = document.getElementById('crop-btn');
   const modal3 = document.getElementById('modal3');
   const mask = document.querySelector('.mask');
   const item_form = document.getElementById('item_form');
-  let image = document.getElementById('item_image');
+  let item_image = document.getElementById('item_image');
+  const cropper_area = document.getElementById('cropper-area');
+
   let image_viw = document.getElementById('cropper-img');
 
-  let dataX = document.getElementById('dataX');
-  let dataY = document.getElementById('dataY');
-  let dataWidth = document.getElementById('dataWidth');
-  let dataHeight = document.getElementById('dataHeight');
+  let fileName;
 
 
-  image.addEventListener('change', function(e){
+  item_form.addEventListener('change', function(e){
     modal3.classList.remove('hidden');
     mask.classList.remove('hidden');
-    URL.revokeObjectURL(image_viw.src);
-    image_viw.src = URL.createObjectURL(image.files[0]);
+    let file = e.target.files[0];
+    fileName = file.name;
 
-  let options = {
-    dragmode: 'crop',
-    aspectRatio: 1.5/1,
-    restore: false,
-    guides: false,
-    center: false,
-    highlight: true,
-    cropBoxMovable: true,
-    cropBoxResizable: true,
-    modal: true,
-    crop: (e) => {
-      dataX.value = (Math.round(e.detail.x));
-      dataY.value = (Math.round(e.detail.y));
-      dataWidth.value = (Math.round(e.detail.width));
-      dataHeight.value = (Math.round(e.detail.height));
+    let reader = new FileReader();
+
+    if(file.type.indexOf('image')<0){
+      return false;
     }
-  };
 
-    let cropper = new Cropper(cropperImg, options);
-    let blob;
-    let fileName = image.files[0].name;
+    reader.onload = (function(file){
+      return function(event){
+        console.log(cropper_area);
+        cropper_area.innerHTML = "";
+        let cropped_image = document.createElement('img');
+        console.log(cropped_image);
+        cropped_image.src = event.target.result;
+        cropped_image.id = "crop_image";
+        cropped_image.title = file.name;
+        cropper_area.appendChild(cropped_image);
+        initCrop();
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
 
-    btn.addEventListener('click', function(){
-      // image_file_field の中身を削除
-      image.value = '';
-      const croppedCanvas = cropper.getCroppedCanvas();
+    let cropper;
+    function initCrop() {
+      cropper = new Cropper(crop_image,{
+        dragmode: 'move',
+        aspectRatio: 1.5/1,
+        restore: false,
+        guides: false,
+        center: false,
+        highlight: false,
+        cropBoxMovable: false,
+        cropBoxResizable: false,
+        modal: true,
+        ready: function(){
+          croppable = true;
+        }
+      });
+        // 初回表示時
+      crop_image.addEventListener('ready', function(e){
+        cropping(e);
+      });
+      // 画像をドラッグした際の処理
+      crop_image.addEventListener('cropend', function(e){
+        cropping(e);
+      });
+      // 画像を拡大・縮小した際の処理
+      crop_image.addEventListener('zoom', function(e){
+        cropping(e);
+      });
+    }
+
+    let croppedCanvas;
+    function cropping(e) {
+      croppedCanvas = cropper.getCroppedCanvas();
       let resultImgUrl = croppedCanvas.toDataURL();
       // let resultImgBlob = croppedCanvas.toBlob;
       let result = document.getElementById('cpi');
       result.src = resultImgUrl;
+    }
 
-      croppedCanvas.toBlob(function(b) {
-        console.log(b);
-        let formData = new FormData(item_form);
-        formData.append('image', b, 'picture.png');
-        console.log(formData);
-      });
-
-      // フォーム要素append type file input タグを追加
-      // let formData = new FormData(document.forms[0]);
-      // formData.append('data1', blob,'image.png');
-
+    btn.addEventListener('click', function(){
       modal3.classList.add('hidden');
       mask.classList.add('hidden');
     });
+
+  $('#submit').on('click',function(){
+    console.log("AAA");
+    croppedCanvas.toBlob(function(blob) {
+      const blob_file = new FileReader([blob], fileName);
+      let formData = new FormData();
+      formData.append('image', blob_file);
+
+      $.ajax({
+        url: '/items',
+        type: 'post',
+        catch: false,
+        dataType: 'json',
+        processData: false,
+        data: formData,
+      })
+      .done(function(response){
+        console.debug("result" + response);
+      })
+      .fail(function(xhr){
+        alert('失礼しました');
+        console.debug("result" + xhr);
+      });
+    });
   });
-
-
-
-
-
-  // let options = {
-  //   dragmode: 'crop',
-  //   aspectRatio: 1/1,
-  //   restore: false,
-  //   guides: false,
-  //   center: false,
-  //   highlight: true,
-  //   cropBoxMovable: true,
-  //   cropBoxResizable: true,
-  //   modal: true,
-  //   crop: (e) => {
-  //     dataX.val(Math.round(e.detail.x));
-  //     dataY.val(Math.round(e.detail.y));
-  //     dataWidth.val(Math.round(e.detail.width));
-  //     dataHeight.val(Math.round(e.detail.height));
-  //   }
-  // };
-
-  // // when file upload
-  // img_field.change((e) => {
-  //   image.cropper('destroy').removeAttr('src');
-  //   let file = e.target.files[0];
-  //   let reader = new FileReader();
-  //   if (file.type.indexOf('image') < 0) {
-  //     window.alert("画像を選択してください");
-  //     return ;
-  //   }
-  //   reader.onload = ((e) => {
-  //     image.attr('src',"");
-  //     image.attr('src', e.target.result);
-  //     cropModal.modal('show');
-  //     cropModal.on('shown.bs.modal', () => {
-  //     image.cropper(options);
-  //     });
-  //   });
-  //   reader.readAsDataURL(file);
-  // });
-  // // onclick save button
-  // button.click(() => {
-  //   imgCropping();
-  // });
-
-  // // modalを閉じたとき、cropper要素を初期化
-  // cropModal.on('hidden.bs.modal',function() {
-  //   $image.cropper('destroy').removeAttr('src');
-  //   let $cropperContainer = $('.cropper-container');
-  //   $cropperContainer.remove();
-  // });
-
-  // function imgCropping() {
-  //   if (!croppable) {
-  //     alert('トリミングする画像が用意されていません');
-  //     return false;
-  //   }
-  //   beforeUpload.hide();
-  //   let croppedData = $image.cropper('getCroppedCanvas').toDataURL();
-  //   croppedImage.attr('src', croppedData);
-  //   cropModal.modal('hide');
-  // }
-});
+})
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,10 +3,6 @@ class Public::ItemsController < ApplicationController
     @item = Item.new(item_params)
     @item.user_id = current_user.id
     @item.save
-    session[:crop_x] = item_params[:x]
-    session[:crop_y] = item_params[:y]
-    session[:crop_width] = item_params[:width]
-    session[:crop_height] = item_params[:height]
     redirect_to user_path current_user
   end
 
